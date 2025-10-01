b08ddfda442ad4198a4c4d16a530400d084287bd
SananAbdulla <sananabdulla@gmail.com>
2025-10-01 16:55:12 +0400

    first commit

--- src/test/java/loopcamp/StepDefs.java
+++ src/test/java/loopcamp/StepDefs.java
@@ -1,6 +1,7 @@
 package loopcamp;
 
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -19,10 +20,17 @@ import java.time.Duration;
 
 public class StepDefs {
 
+    @Before
+    public void setUp() {
+        // Initialize driver before each scenario
+        Driver.getDriver();
+        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        Driver.getDriver().manage().window().maximize();
+    }
+
     @Given("^I am on the home page$")
     public void i_am_on_the_home_page() {
         // Open eBay home page
-        Driver.getDriver().manage().window().maximize();
         Driver.getDriver().get("https://www.ebay.com");
     }
 
@@ -34,7 +42,7 @@ public class StepDefs {
     }
 
     @Then("^I should see the results for \"([^\"]*)\"$")
-    public void i_should_see_the_results(String expectedKeyword) {
+    public void i_should_see_the_results_for(String expectedKeyword) {
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(20));
 
         // Scroll down to make sure elements are visible (headless-safe)
@@ -55,6 +63,24 @@ public class StepDefs {
         );
     }
 
+    // ADD THIS METHOD TO FIX THE UNDEFINED STEP ERROR
+    @Then("I should see the results")
+    public void i_should_see_the_results() {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(20));
+
+        // Wait for the results heading to appear
+        WebElement heading = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath("//h1[contains(@class,'srp-controls__count-heading')]")
+                )
+        );
+
+        // Simple assertion that results are displayed
+        Assert.assertTrue("Search results should be visible", heading.isDisplayed());
+
+        System.out.println("Search results heading: " + heading.getText());
+    }
+
     @After
     public void tearDown(Scenario scenario) {
         if (scenario.isFailed()) {
@@ -64,4 +90,3 @@ public class StepDefs {
         Driver.closeDriver();
     }
 }
-
