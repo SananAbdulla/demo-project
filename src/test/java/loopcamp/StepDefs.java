e816c0f6a481506659b37f763b48b71f0d417534
SananAbdulla <sananabdulla@gmail.com>
2025-10-02 02:52:26 +0400

    first commit

--- src/test/java/loopcamp/StepDefs.java
+++ src/test/java/loopcamp/StepDefs.java
@@ -1,3 +1,80 @@
+//package loopcamp;
+//
+//import io.cucumber.java.After;
+//import io.cucumber.java.Scenario;
+//import io.cucumber.java.en.Given;
+//import io.cucumber.java.en.Then;
+//import io.cucumber.java.en.When;
+//import org.junit.Assert;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.Keys;
+//import org.openqa.selenium.OutputType;
+//import org.openqa.selenium.TakesScreenshot;
+//
+//import java.util.concurrent.TimeUnit;
+//
+//public class StepDefs {
+//
+//    @Given("^I am on the home page$")
+//    public void i_am_on_the_home_page() throws Throwable {
+//        Driver.getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//        Driver.getDriver().manage().window().maximize();
+//        Driver.getDriver().get("https://www.ebay.com");
+//    }
+//
+//    @When("^I search for \"([^\"]*)\"$")
+//    public void i_search_for(String search) throws Throwable {
+//        Driver.getDriver().findElement(By.xpath("//input[@name='_nkw']")).sendKeys(search + Keys.ENTER);
+//    }
+//
+//    @Then("^I should see the results$")
+//    public void i_should_see_the_results() throws Throwable {
+//        Thread.sleep(3000); // Increased wait time
+//
+//        // OPTION 1: Check URL (most reliable)
+//        String currentUrl = Driver.getDriver().getCurrentUrl();
+//        Assert.assertTrue("Should be on search results page. URL: " + currentUrl,
+//                currentUrl.contains("ebay.com/sch/"));
+//
+//        // OPTION 2: Try to find results text
+//        try {
+//            String resultsText = Driver.getDriver().findElement(By.xpath("//span[contains(text(), 'results for')]")).getText().toLowerCase();
+//            Assert.assertTrue("Search results should contain 'glass teapot'. Found: " + resultsText,
+//                    resultsText.contains("glass teapot"));
+//        } catch (Exception e) {
+//            // If element not found, at least we verified the URL
+//            System.out.println("Results element not found, but URL verification passed");
+//        }
+//    }
+//
+//    @Then("^I should see more results$")
+//    public void i_should_see_more_results() throws Throwable {
+//        Thread.sleep(3000); // Increased wait time
+//
+//        // Same approach for second scenario
+//        String currentUrl = Driver.getDriver().getCurrentUrl();
+//        Assert.assertTrue("Should be on search results page. URL: " + currentUrl,
+//                currentUrl.contains("ebay.com/sch/"));
+//
+//        try {
+//            String resultsText = Driver.getDriver().findElement(By.xpath("//span[contains(text(), 'results for')]")).getText().toLowerCase();
+//            Assert.assertTrue("Search results should contain 'useless box'. Found: " + resultsText,
+//                    resultsText.contains("useless box"));
+//        } catch (Exception e) {
+//            System.out.println("Results element not found, but URL verification passed");
+//        }
+//    }
+//
+//    @After
+//    public void tearDown(Scenario scenario) {
+//        if (scenario.isFailed()) {
+//            final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+//            scenario.attach(screenshot, "image/png", scenario.getName());
+//        }
+//        Driver.closeDriver();
+//    }
+//}
+
 package loopcamp;
 
 import io.cucumber.java.After;
@@ -13,56 +90,41 @@ import org.openqa.selenium.TakesScreenshot;
 
 import java.util.concurrent.TimeUnit;
 
+
 public class StepDefs {
 
     @Given("^I am on the home page$")
     public void i_am_on_the_home_page() throws Throwable {
         Driver.getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         Driver.getDriver().manage().window().maximize();
-        Driver.getDriver().get("https://www.ebay.com");
+//        Driver.getDriver().get("https://www.etsy.com");
+//        Driver.getDriver().get("http://www.amazon.com");
+        Driver.getDriver().get("http://www.ebay.com");
     }
 
     @When("^I search for \"([^\"]*)\"$")
     public void i_search_for(String search) throws Throwable {
-        Driver.getDriver().findElement(By.xpath("//input[@name='_nkw']")).sendKeys(search + Keys.ENTER);
+//        Driver.getDriver().findElement(By.cssSelector("input[aria-label^='Search']")).sendKeys(search + Keys.ENTER);
+//        Driver.getDriver().findElement(By.xpath("//*[@id='twotabsearchtextbox']")).sendKeys(search + Keys.ENTER);
+        Driver.getDriver().findElement(By.xpath("//input[@name=\'_nkw\']")).sendKeys(search + Keys.ENTER);
+
     }
 
     @Then("^I should see the results$")
     public void i_should_see_the_results() throws Throwable {
-        Thread.sleep(3000); // Increased wait time
-
-        // OPTION 1: Check URL (most reliable)
-        String currentUrl = Driver.getDriver().getCurrentUrl();
-        Assert.assertTrue("Should be on search results page. URL: " + currentUrl,
-                currentUrl.contains("ebay.com/sch/"));
-
-        // OPTION 2: Try to find results text
-        try {
-            String resultsText = Driver.getDriver().findElement(By.xpath("//span[contains(text(), 'results for')]")).getText().toLowerCase();
-            Assert.assertTrue("Search results should contain 'glass teapot'. Found: " + resultsText,
-                    resultsText.contains("glass teapot"));
-        } catch (Exception e) {
-            // If element not found, at least we verified the URL
-            System.out.println("Results element not found, but URL verification passed");
-        }
+        Thread.sleep(2000);
+//        Assert.assertTrue(Driver.getDriver().findElement(By.cssSelector("span[class^='a-color-state']")).getText().contains("wooden loop"));
+//        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("search"));
+        Assert.assertTrue(Driver.getDriver().findElement(By.xpath("(//h1[@class='srp-controls__count-heading']//span)[2]")).getText().contains("glass teapot"));
     }
 
     @Then("^I should see more results$")
     public void i_should_see_more_results() throws Throwable {
-        Thread.sleep(3000); // Increased wait time
+        Thread.sleep(2000);
+//        Assert.assertTrue(Driver.getDriver().findElement(By.cssSelector("span[class^='a-color-state']")).getText().contains("useless box"));
+//        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("search"));
+        Assert.assertTrue(Driver.getDriver().findElement(By.xpath("(//h1[@class='srp-controls__count-heading']//span)[2]")).getText().contains("useless box"));
 
-        // Same approach for second scenario
-        String currentUrl = Driver.getDriver().getCurrentUrl();
-        Assert.assertTrue("Should be on search results page. URL: " + currentUrl,
-                currentUrl.contains("ebay.com/sch/"));
-
-        try {
-            String resultsText = Driver.getDriver().findElement(By.xpath("//span[contains(text(), 'results for')]")).getText().toLowerCase();
-            Assert.assertTrue("Search results should contain 'useless box'. Found: " + resultsText,
-                    resultsText.contains("useless box"));
-        } catch (Exception e) {
-            System.out.println("Results element not found, but URL verification passed");
-        }
     }
 
     @After
@@ -72,5 +134,10 @@ public class StepDefs {
             scenario.attach(screenshot, "image/png", scenario.getName());
         }
         Driver.closeDriver();
+
+
+
+
     }
+
 }
