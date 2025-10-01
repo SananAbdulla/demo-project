a53127d25f74fb3b26c5d40c6b90600dbe10e6f9
SananAbdulla <sananabdulla@gmail.com>
2025-10-01 12:50:07 +0400

    first commit

--- src/test/java/loopcamp/Driver.java
+++ src/test/java/loopcamp/Driver.java
@@ -132,7 +132,7 @@ public class Driver {
                 case "remote-chrome-linux":
                     try {
                         // assign your grid server address
-                        String gridAddress = "34.227.172.25";
+                        String gridAddress = "34.224.27.248";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         chromeOptions = new ChromeOptions();
@@ -158,7 +158,7 @@ public class Driver {
                 case "remote-firefox-linux":
                     try {
                         // assign your grid server address
-                        String gridAddress = "34.227.172.25";
+                        String gridAddress = "34.224.27.248";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("firefox");
