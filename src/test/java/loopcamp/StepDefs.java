cd6534193aec88ae851270860072310165993d23
SananAbdulla <sananabdulla@gmail.com>
2025-10-01 15:12:55 +0400

    first commit

--- src/test/java/loopcamp/StepDefs.java
+++ src/test/java/loopcamp/StepDefs.java
@@ -10,43 +10,55 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
-
 
 public class StepDefs {
 
     @Given("^I am on the home page$")
-    public void i_am_on_the_home_page() throws Throwable {
+    public void i_am_on_the_home_page() {
         Driver.getDriver().manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         Driver.getDriver().manage().window().maximize();
-//        Driver.getDriver().get("https://www.etsy.com");
-//        Driver.getDriver().get("http://www.amazon.com");
-        Driver.getDriver().get("http://www.ebay.com");
+        Driver.getDriver().get("https://www.ebay.com");
     }
 
     @When("^I search for \"([^\"]*)\"$")
-    public void i_search_for(String search) throws Throwable {
-//        Driver.getDriver().findElement(By.cssSelector("input[aria-label^='Search']")).sendKeys(search + Keys.ENTER);
-//        Driver.getDriver().findElement(By.xpath("//*[@id='twotabsearchtextbox']")).sendKeys(search + Keys.ENTER);
-        Driver.getDriver().findElement(By.xpath("//input[@name=\'_nkw\']")).sendKeys(search + Keys.ENTER);
+    public void i_search_for(String search) {
+        Driver.getDriver().findElement(By.xpath("//input[@name='_nkw']")).sendKeys(search + Keys.ENTER);
     }
 
     @Then("^I should see the results$")
-    public void i_should_see_the_results() throws Throwable {
-        Thread.sleep(15000);
-//        Assert.assertTrue(Driver.getDriver().findElement(By.cssSelector("span[class^='a-color-state']")).getText().contains("wooden loop"));
-//        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("search"));
-        Assert.assertTrue(Driver.getDriver().findElement(By.xpath("(//h1[@class='srp-controls__count-heading']//span)[2]")).getText().contains("glass teapot"));
+    public void i_should_see_the_results() {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(15));
+
+        WebElement keywordSpan = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath("//h1[@class='srp-controls__count-heading']//span[@class='BOLD']")
+                )
+        );
+
+        String actualText = keywordSpan.getText().toLowerCase();
+        Assert.assertTrue("Search results did not contain expected keyword. Found: " + actualText,
+                actualText.contains("glass teapot"));
     }
 
     @Then("^I should see more results$")
-    public void i_should_see_more_results() throws Throwable {
-        Thread.sleep(15000);
-//        Assert.assertTrue(Driver.getDriver().findElement(By.cssSelector("span[class^='a-color-state']")).getText().contains("useless box"));
-//        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("search"));
-        Assert.assertTrue(Driver.getDriver().findElement(By.xpath("(//h1[@class='srp-controls__count-heading']//span)[2]")).getText().contains("useless box"));
+    public void i_should_see_more_results() {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(15));
 
+        WebElement keywordSpan = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath("//h1[@class='srp-controls__count-heading']//span[@class='BOLD']")
+                )
+        );
+
+        String actualText = keywordSpan.getText().toLowerCase();
+        Assert.assertTrue("Search results did not contain expected keyword. Found: " + actualText,
+                actualText.contains("useless box"));
     }
 
     @After
@@ -56,10 +68,5 @@ public class StepDefs {
             scenario.attach(screenshot, "image/png", scenario.getName());
         }
         Driver.closeDriver();
-
-
-
-
     }
-
 }
